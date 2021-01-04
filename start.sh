8cf997d3a2b5148a0ba9b537c132a024bef034a2
kylin <kylin@gmail.com>
2021-01-04 12:48:16 +0800

    :sparkles: feat(1.0):
    Signed-off-by: kylin <kylin@gmail.com>

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 source venv/bin/activate
-nohup uvicorn main:app --host 0.0.0.0 --port 80 > system.log 2>&1 &
+nohup uvicorn main:app --host 0.0.0.0 --port 8080 > system.log 2>&1 &
 deactivate
